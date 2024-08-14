5645cb95038fb77c642b74110cf7d6819ff8a3ba
Richard <richardmusime6@gmail.com>
2024-08-14 08:49:45 +0200

    Added profile and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css'
+import Profile from '../components/Profile'
+import Login from '../components/Login'
 
 function App() {
   return (
   <>
-  <h1 className="text-3xl font-bold underline">
-      Hello world!
+  <h1 className="text-2xl font-bold">
+      <Profile/>
+      <Login/>
     </h1>
   </>
   )
